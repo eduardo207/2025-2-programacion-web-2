9dd0e4814e592a89d5abbe3761493fe76c8a9d4a
Jerson Eduardo Quiñonez <eduardo207@gmail.com>
2025-07-18 18:04:57 -0600

    Versionamiento de ejercicios de clase
    Versionamiento de ejercicios de clase

---
+++ ejercicio-npm/index.js
@@ -0,0 +1,14 @@
+import chalk from 'chalk';
+
+console.log('Hola mundo, este es mi primer ejercicio con npm');
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.red('¡Hola mundo!'));
+console.log(chalk.green('¡Hola mundo!'));
+console.log(chalk.yellow('¡Hola mundo!'));
+console.log(chalk.magenta('¡Hola mundo!'));
+console.log(chalk.cyan('¡Hola mundo!'));
+console.log(chalk.white('¡Hola mundo!'));
+console.log(chalk.gray('¡Hola mundo!'));
+console.log(chalk.bgBlue('¡Hola mundo!'));
+console.log(chalk.bgRed('¡Hola mundo!'));
