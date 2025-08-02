e368a9258b3ce3df8f98b1c06dd4b8640ea6a995
Jerson Eduardo Quiñonez <eduardo207@gmail.com>
2025-08-01 19:30:40 -0600

    API de vehiculos
    API de vehiculos

--- ejercicio-api/routes/index.route.js
+++ ejercicio-api/routes/index.route.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import clientsRoute from './clients.route.js';
+import vehiclesRoute from './vehicles.route.js';
 
 const router = Router();
 
 router.use("/clients", clientsRoute);
+router.use("/vehicles", vehiclesRoute);
 
 export default router;
