1a81e22ca46f73f23218467098643eb746d31436
Jerson Eduardo Quiñonez <eduardo207@gmail.com>
2025-08-01 18:03:11 -0600

    Actualización de ejemplos de clase
    Actualización de ejemplos de clase

--- ejercicio-api/server.js
+++ ejercicio-api/server.js
@@ -7,55 +7,101 @@ app.use(express.json());
 
 // Ruta para obtener todos los clientes
 app.get('/api/clients',(req, res) => {
-  let response = {
-    message: "success",
-    data: {
-      clients: clients,
-      count: clients.length
-    }
-  };
-  res.status(200).json(response);
+  try{
+    let response = {
+      message: "success",
+      data: {
+        clients: clients,
+        count: clients.length
+      }
+    };
+    res.status(200).json(response);
+  }catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Error interno del servidor"
+    });
+  }
 })
 
 // Ruta para obtener un cliente por PARAM ID
 app.get('/api/clients/:id',(req, res) => {
-  // obtención por param id
-  const id = req.params.id;
-  // obtención por query id
-  // const id = req.query.id;
-  const client = clients.find(c => c.id == id);
-  let response = {};
-  if (!client){
-    response = {
-      message: "cliente no encontrado"
+  try{
+    // obtención por param id
+    const id = req.params.id;
+    // obtención por query id
+    // const id = req.query.id;
+    const client = clients.find(c => c.id == id);
+    let response = {};
+    if (!client){
+      response = {
+        message: "cliente no encontrado"
+      }
+      return res.status(404).json(response);
     }
-    return res.status(404).json(response);
+
+    response = {
+      message: "success",
+      data: client
+    };
+
+    return res.status(200).json(response);
+  }catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Error interno del servidor"
+    });
   }
-
-  response = {
-    message: "success",
-    data: client
-  };
-
-  return res.status(200).json(response);
 })
 
 // Ruta opara implementar el POST de client
 app.post('/api/clients',(req, res) => {
-  const newClient = req.body;
-  
-  newClient.id = Number(`${Date.now()}`);
-  clients.push(newClient);
-
-  let response = {
-    message: "success",
-    data: {
-      clientId: newClient.id,
+  try{
+    const newClient = req.body;
+    const client = clients.find(c => c.id == newClient.id);
+    let response = {};
+    if (client){
+      response = {
+        message: "cliente ya existe",
+      }
+      return res.status(409).json(response);
     }
+
+    if (newClient.nombre === undefined || newClient.nombre === "") {
+      response = {
+        message: "Se requiere el nombre del cliente",
+      }
+      return res.status(400).json(response);
+    }
+
+    clients.push(newClient);
+
+    response = {
+      message: "success",
+      data: {
+        clientId: newClient.id,
+      }
+    }
+    return res.status(201).json(response);
+  }catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Error interno del servidor"
+    });
   }
-  return res.status(201).json(response);
 })
 
+// Ruta opara actualizar un cliente
+app.put('/api/clients',(req, res) => {
+  try{
+    throw new Error("Error de prueba");
+  }catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Error interno del servidor"
+    });
+  }
+})
 app.listen(port, () => console.log('Servidor iniciado'));
 
 
