a47cbf4bd990f98c637010b7248df2286e167937
Jerson Eduardo Quiñonez <eduardo207@gmail.com>
2025-08-19 19:21:44 -0600

    commit de avances
    commit de avances

--- ejemplo-mongodb/server.js
+++ ejemplo-mongodb/server.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import routes from './routes/index.route.js';
+import dotenv from 'dotenv';
+import { connectToMongo } from './configs/mongodb.config.js';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 app.use(express.json());
 
 app.use("/",routes);
 
-app.listen(port, () => console.log('Servidor iniciado'));
+const startServer = async () => {
+  try {
+    await connectToMongo();
+    app.listen(port, () => console.log('Servidor iniciado'));
+  }catch (error) {
+    console.error("Error al iniciar el servidor:", error);  
+  }
+}
+
+startServer();
