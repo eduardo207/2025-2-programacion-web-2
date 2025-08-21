473371a56590e9d9877547e97c18c9ba821e1dd3
Jerson Eduardo Quiñonez <eduardo207@gmail.com>
2025-08-20 18:47:56 -0600

    Ejercicio de clase de mongoDB
    Ejercicio de clase de mongoDB

--- ejemplo-mongodb/controllers/clientes.controller.js
+++ ejemplo-mongodb/controllers/clientes.controller.js
@@ -1,13 +1,17 @@
+import { responseSuccess, responseError } from '../helpers/response.helper.js';
+import { getDb } from '../configs/mongodb.config.js';
+
 //Handler para el metodo get de todos los clientes
 const getClientesHandler = async (req, res) => {
   try{
-    let response = {
-      message: "success",
-      data: {
-        clients: clients,
-        count: clients.length
-      }
-    };
+    const db = getDb();
+    const clientes = await db.collection('clientes').find().toArray();
+    
+    if (!clientes)
+      return res.status(404).json(responseError("No se encontraron clientes"));
+
+    const response = responseSuccess("Clientes obtenidos exitosamente",clientes);
+
     res.status(200).json(response);
   }catch (error) {
     console.error(error);
